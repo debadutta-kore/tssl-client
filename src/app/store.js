ef534c7cd22c67d53fe0dceb770f30a2053ff9ca
debaduttakore <139857436+debaduttakore@users.noreply.github.com>
2023-10-20 18:13:17 +0530

    resolve deployement error

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,7 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import usecaseReducer from "./features/usecaseSlice";
-import authReducer from "./features/useAuthSlice";
-import usersReducer from "./features/usersSlice";
+import usecaseReducer from "./features/usecaseSlice.js";
+import authReducer from "./features/useAuthSlice.js";
+import usersReducer from "./features/usersSlice.js";
 const rootReducer = combineReducers({
   auth: authReducer,
   users: usersReducer,

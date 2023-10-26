8775d93eaf9c8c0d24ca278c00d792a1ca0344d9
debaduttakore <139857436+debaduttakore@users.noreply.github.com>
2023-10-26 10:34:07 +0530

    add comming soon status on usecase screen

--- src/components/card/usecaseSetting/index.jsx
+++ src/components/card/usecaseSetting/index.jsx
@@ -10,56 +10,57 @@ import { updateUsecase } from "../../../app/features/usecaseSlice";
 import { toast } from "react-toastify";
 import { unwrapResult } from "@reduxjs/toolkit";
 function UsecaseSetting(props) {
-  const ref = useRef();
-  const dispatch = useDispatch();
-  const [isDelete, setIsDelete] = useState(false);
+    const ref = useRef();
+    const dispatch = useDispatch();
+    const [isDelete, setIsDelete] = useState(false);
 
-  const info = usecaseDb.find((usecase) => usecase.id === props.usecaseId);
-  const onChangeStatus = () => {
-    const status = ref.current.checked ? "enabled" : "disabled";
-    dispatch(
-      updateUsecase({
-        id: info.id,
-        enable: ref.current.checked,
-      })
-    )
-      .then(unwrapResult)
-      .then(() => {
-        if (ref.current.checked) {
-          toast("Use Case enabled successfully!", { type: "success" });
-        } else {
-          toast(`Use Case disabled successfully!`, { type: "warning" });
-        }
-      })
-      .catch(() => {
-        toast(`Unable to ${status} ${info.name} usecase`, { type: "error" });
-      });
-  };
+    const info = usecaseDb.find((usecase) => usecase.id === props.usecaseId);
+    const onChangeStatus = () => {
+        const status = ref.current.checked ? "enabled" : "disabled";
+        dispatch(
+            updateUsecase({
+                id: info.id,
+                enable: ref.current.checked,
+            })
+        )
+            .then(unwrapResult)
+            .then(() => {
+                if (ref.current.checked) {
+                    toast("Use Case enabled successfully!", { type: "success" });
+                } else {
+                    toast(`Use Case disabled successfully!`, { type: "warning" });
+                }
+            })
+            .catch(() => {
+                toast(`Unable to ${status} ${info.name} usecase`, { type: "error" });
+            });
+    };
 
-  return (
-    <Card className={style["setting-cards"]}>
-      {isDelete && (
-        <ConfirmDeleteUsecase
-          delete={{ id: props.id, name: info.name }}
-          onClose={() => setIsDelete(!isDelete)}
-        />
-      )}
-      <div className={style["card-heading"]}>
-        <div>
-          <span>{info.name}</span>
-        </div>
-        <div>
-          <Switch
-            onChange={onChangeStatus}
-            ref={ref}
-            checked={props.enable === 1 ? true : false}
-          />
-          <button onClick={() => setIsDelete(!isDelete)}>
-            <img src={deleteIcon} alt="delete" width={20} height={20} />
-          </button>
-        </div>
-      </div>
-    </Card>
-  );
+    return (
+        <Card className={style["setting-cards"]}>
+            {isDelete && (
+                <ConfirmDeleteUsecase
+                    delete={{ id: props.id, name: info.name }}
+                    onClose={() => setIsDelete(!isDelete)}
+                />
+            )}
+            <div className={style["card-heading"]}>
+                <div className={style["info"]}>
+                    <span className={style["name"]}>{info.name}</span>
+                    <span className={style["status"]} style={{ display: info.isComingSoon ? 'inline' : 'none' }}>(Coming Soon)</span>
+                </div>
+                <div>
+                    <Switch
+                        onChange={onChangeStatus}
+                        ref={ref}
+                        checked={props.enable === 1 ? true : false}
+                    />
+                    <button onClick={() => setIsDelete(!isDelete)}>
+                        <img src={deleteIcon} alt="delete" width={20} height={20} />
+                    </button>
+                </div>
+            </div>
+        </Card>
+    );
 }
 export default UsecaseSetting;
